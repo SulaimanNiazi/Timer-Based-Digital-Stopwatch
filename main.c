787c8dd26f12691e38d6f425ed7c700f633f3a27
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-02-25 18:01:11 +0500

    Add rx function for UART transmition

--- main.c
+++ main.c
@@ -9,9 +9,25 @@
 
 #include <xc.h>
 
+void tx(unsigned char value){
+    TXREG = value;
+    while(!TXIF);
+    TXIF = 0;
+}
+
 void main(void){
+    TRISCbits.TRISC6 = 0;
+    TRISCbits.TRISC7 = 1;
+    PORTCbits.RC6 = 0;
+    TRISBbits.TRISB0 = 1;
+    TXSTA = 0x26;
+    RCSTA = 0x90;
+    SPBRG = 0x81;
+    TXIE = 1;
+    TXIF=0;
     while(1){
-        
+        tx('S');
+        tx('N');
     }
     return;
 }
