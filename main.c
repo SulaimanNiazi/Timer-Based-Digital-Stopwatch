5bb0845c7fcd9fdd3f135fcbd598726ada4b6a2b
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-02-25 15:59:11 +0500

    Add main.c file

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   main.c
+ * Author: SulaimanNiazi
+ *
+ * Created on February 25, 2025, 3:38 PM
+ */
+
+#define _XTAL_FREQ 20000000
+
+#include <xc.h>
+
+void main(void){
+    while(1){
+        
+    }
+    return;
+}
